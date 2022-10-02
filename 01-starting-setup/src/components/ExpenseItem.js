04e8fbdf2e43ba53638fa07c568f39f8d0505797
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-02 18:46:04 +0530

    Initial commit

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h1>Expense Item!</h1>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
+
+export default ExpenseItem;
