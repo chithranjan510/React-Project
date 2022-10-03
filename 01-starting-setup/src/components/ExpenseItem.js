733e825bcc2449b9f3e6f2ff0e36a5d707dc6e0e
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-03 22:02:20 +0530

    Assignment 1 : adding expenses component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails"
-import './ExpenseItem.css'
-
+import ExpenseDetails from "./ExpenseDetails";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}/>
-      <ExpenseDetails title={props.title} amount={props.amount}/>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails title={props.title} amount={props.amount} />
     </div>
   );
 }
