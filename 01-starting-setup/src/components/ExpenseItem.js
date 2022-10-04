b6eabd48634df9c77267055fcfa6c4d754a94513
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-04 11:40:21 +0530

    Reducing redundant code using a wrapper component i.e Card

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <ExpenseDetails title={props.title} amount={props.amount} />
-    </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">Rs {props.amount}</div>
+      </div>
+    </Card>
   );
 }
 
