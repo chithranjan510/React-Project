583172e4dca1a131a5fb038b4419c62aa61595ae
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-05 19:50:34 +0530

    Adding a button and an event Listener

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,16 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  const clickHandler = () => {
+    console.log('Clicked!!!')
+  }
+
+  const deleteHandler = (e) => {
+    e.target.parentElement.remove();
+    console.log('Deleted!!!')
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +20,8 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">Rs {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete Item</button>
     </Card>
   );
 }
