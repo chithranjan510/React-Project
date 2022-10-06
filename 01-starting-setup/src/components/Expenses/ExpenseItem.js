1f09cafa5c2b258349b9abea08039f7b341ce5be
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-06 17:55:38 +0530

    adding useState to create state and update the changes in UI

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,27 +1,33 @@
+import {useState} from 'react'
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
+  const [title, setTitle] = useState(props.title);
+  const [expense, setExpense] = useState(props.amount);
+
   const clickHandler = () => {
-    console.log('Clicked!!!')
+    setTitle('Upadeted!!!');
+    // console.log(title);
   }
 
-  const deleteHandler = (e) => {
-    e.target.parentElement.remove();
-    console.log('Deleted!!!')
+  const expenseClickHandler = () => {
+    setExpense('100');
+    // console.log(title);
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">Rs {props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">Rs {expense}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteHandler}>Delete Item</button>
+      <button onClick={expenseClickHandler}>Change Expense</button>
     </Card>
   );
 }
