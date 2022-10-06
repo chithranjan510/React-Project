f2966616aaa23f200726652e30c7fab89c865627
chithranjan kumar <kumarchithranjan@gmail.com>
2022-10-06 21:09:25 +0530

    Adding form and event listeners to take input from the user

---
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,40 @@
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+  }
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" onChange={titleChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="10" step="10" onChange={amountChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
